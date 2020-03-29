653b90e7df5c6318773297191530f651f4134697
Rubby Casallas <rcasalla@users.noreply.github.com>
2020-03-28 19:54:38 -0500

    Cambio comillas dobles por sencillas

--- src/app/course/course.service.ts
+++ src/app/course/course.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from "rxjs";
+import { Observable } from 'rxjs';
 import { Course } from './course';
 
 import { environment } from '../../environments/environment';
